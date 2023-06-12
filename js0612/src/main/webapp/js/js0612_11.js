561180da3b6d1ce10f2a067af61a72886c9dfa72
005 <005@DESKTOP-9H7317A>
2023-06-12 18:15:38 +0900

    6월12일 자바스크립트

--- js0612/src/main/webapp/js/js0612_11.js
+++ js0612/src/main/webapp/js/js0612_11.js
@@ -2,9 +2,12 @@
  * 
  */
 var htmlData = "";
-htmlData += "<tr>";
-htmlData += "<td>aaa</td>";
-htmlData += "<td>1111</td>";
-htmlData += "</tr>";
+
+for(var i=0;i<10;i++){
+	htmlData += "<tr>";
+	htmlData += "<td>aaa</td>";
+	htmlData += "<td>1111</td>";
+	htmlData += "</tr>";
+}
  
 document.getElementById("d01").innerHTML = htmlData;
