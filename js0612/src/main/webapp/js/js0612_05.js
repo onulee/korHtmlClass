93aeac5b930ad34692f4aea325a03896749cb2f7
005 <005@DESKTOP-9H7317A>
2023-06-12 13:10:50 +0900

    6월12일 자바스크립트

---
+++ js0612/src/main/webapp/js/js0612_05.js
@@ -0,0 +1,26 @@
+var input = Number(prompt("숫자를 입력하세요.","예시 : 20"));
+
+if(input%2==0){
+	document.write("짝수입니다.");
+}else{
+	document.write("홀수입니다.");
+	
+}
+
+/*
+var input = Number(prompt("1번째 숫자를 입력하세요.")); //20
+var input2 = Number(prompt("2번째 숫자를 입력하세요.")); //10
+
+document.write(input+input2); //2010
+document.write("<br>"); 
+
+document.write(input-input2); //2010
+*/
+
+/*
+if(input>100){
+	document.write("100보다 큰수입니다.");
+}else{
+	document.write("100보다 작은수 입니다.");
+}
+*/
