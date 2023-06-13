9cb61c35717ff0d8ee6c9480f86644f3df4d77a3
005 <005@DESKTOP-9H7317A>
2023-06-14 03:00:22 +0900

    퇴근보다우선해야할커밋

--- test0612/src/main/java/com/java/www/BirthConfirm.java
+++ test0612/src/main/java/com/java/www/BirthConfirm.java
@@ -2,6 +2,8 @@ package com.java.www;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Calendar;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,34 +12,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/StuInput")
-public class StuInput extends HttpServlet {
+@WebServlet("/BirthConfirm")
+public class BirthConfirm extends HttpServlet {
 	
 	protected void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
+		//form 모든데이터 -> request
+		request.setCharacterEncoding("utf-8"); //한글처리
 		System.out.println("doAction");
-		String stuNo = request.getParameter("stuNo");
-		String name = request.getParameter("name");
-		String kor = request.getParameter("kor");
-		String eng = request.getParameter("eng");
-		String math = request.getParameter("math");
-		int total = Integer.parseInt(kor)+Integer.parseInt(eng)+Integer.parseInt(math);
+		String birth = request.getParameter("birth");
+		int num_birth = Integer.parseInt(birth);
+		Date today = new Date(System.currentTimeMillis());
+		int year = today.getYear();
 		
-		System.out.printf("%s,%s,%s,%s,%s,%d \n",stuNo,name,kor,eng,math,total);
+		int age = 2023 - num_birth;
+		System.out.println("년도 : "+year);
+		System.out.println("당신의 나이 : "+age);
+		String content="";
+		if(age>=18) {
+			content="<h2 style='color:blue; font-weight:600;'>주류 판매가 가능합니다.</h2>";
+		}else {
+			content="<h2 style='color:red; font-weight:600;'>미성년자! 주류판매 금지합니다.</h2>";
+		}
 		
 		response.setContentType("text/html; charset=utf-8");
 		PrintWriter writer = response.getWriter();
-		
 		writer.println("<html>");
-		writer.println("<head></head>");
+		writer.println("<head><title>미성년자 확인</title></head>");
 		writer.println("<body>");
-		writer.println("<h2>학생성적</h2>");
-		writer.println("<p>학번 : "+stuNo+"</p>");
-		writer.println("<p>이름 : "+name+"</p>");
-		writer.println("<p>국어 : "+kor+"</p>");
-		writer.println("<p>영어 : "+eng+"</p>");
-		writer.println("<p>수학 : "+math+"</p>");
-		writer.println("<p>합계 : "+total+"</p>");
+		writer.println(content);
 		writer.println("</body>");
 		writer.println("</html>");
 		
@@ -45,14 +47,12 @@ public class StuInput extends HttpServlet {
 		
 		
 		
-	
+		
 	}
-	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    System.out.println("doGet");
+		System.out.println("doGet");
 		doAction(request,response);
 	}
-	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("doPost");
 		doAction(request,response);
