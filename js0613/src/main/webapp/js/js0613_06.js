1224212f78abd5e84eb5590b0ccd4d433634d686
005 <005@DESKTOP-9H7317A>
2023-06-13 18:23:09 +0900

    커밋대신함

--- js0613/src/main/webapp/js/js0613_06.js
+++ js0613/src/main/webapp/js/js0613_06.js
@@ -1,10 +1,24 @@
 /**
  *  시간가져오기
  */
-
- var today = new Date();
- let aaa = 0;
- var bbb = 0;
- var bbb = 1;
+ var a = 0;     //변수
+ let aaa = 0;   //변수 
+ const bbb = 0; //상수
+ 
+ var interval;
+ 
+ function start(){
+	 interval  = setInterval(function(){
+		 var today = new Date();
+		 var hours = today.getHours();
+		 var minutes = today.getMinutes();
+		 var seconds = today.getSeconds();
+		 var seconds2;
+		 if(seconds<10) seconds2 = "0"+seconds;
+		 else seconds2 = seconds;
+		 var millisecondes = today.getMilliseconds();
+		 document.getElementById("h01").innerText = hours+":"+minutes+":"+seconds2;
+	 },1000);
+ }
  
  
