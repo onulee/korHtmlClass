c13e1ab16e2412357c3b251ea3391ce47a9f9bde
005 <005@DESKTOP-9H7317A>
2023-06-19 18:23:46 +0900

    6월19일 jsp수업

--- jsp0619/src/main/java/com/java/www/MemberDao.java
+++ jsp0619/src/main/java/com/java/www/MemberDao.java
@@ -18,17 +18,87 @@ public class MemberDao {
 	PreparedStatement pstmt;
 	ResultSet rs;
 	//컬럼타입
+	
 	String id,pw,name,phone;
 	String query;
 	int result;
+	
+	// 커넥션 연결메소드 - conn
+	private Connection getConnection() {
+		Connection connection = null;
+		try {
+			context = new InitialContext();
+	    	dataSource = (DataSource) context.lookup("java:comp/env/jdbc/Oracle11g");
+	    	connection = dataSource.getConnection();
+		}catch(Exception e){e.printStackTrace();}
+		return connection;
+	}
+	
+	//로그인 확인 메소드 - 성공:1, 실패:0
+	public Member selectLogin(String userId,String userPw) {
+		System.out.println("userId : "+userId);
+		Member member = null;
+		try{
+	    	 conn = getConnection();
+	    	 query = "select * from member where id=? and pw=?";
+	    	 pstmt = conn.prepareStatement(query);
+	    	 pstmt.setString(1, userId);
+	    	 pstmt.setString(2, userPw);
+	    	 rs = pstmt.executeQuery();
+	    	 while(rs.next()) {
+	    		 id = rs.getString("id");
+	    		 pw = rs.getString("pw");
+	    		 name = rs.getString("name");
+	    		 phone = rs.getString("phone");
+	    		 member = new Member(id,pw,name,phone);
+	    	 }
+	    }catch(Exception e){
+	    	e.printStackTrace();
+	    }finally{
+	    	try{
+	    		if(rs!=null) rs.close();
+	    		if(pstmt!=null) pstmt.close();
+	    		if(conn!=null) conn.close();
+	    	}catch(Exception e2){e2.printStackTrace();}
+	    }
+		
+		return member;
+	}//selectLogin
+	
+	//1개 회원정보 검색 메소드
+	public Member selectMemberOne(String sessionId) {
+	    Member member = null;
+	    try{
+	    	 conn = getConnection();
+	    	 query = "select * from member where id=?";
+	    	 pstmt = conn.prepareStatement(query);
+	    	 pstmt.setString(1, sessionId);
+	    	 rs = pstmt.executeQuery();
+	    	 while(rs.next()) {
+	    		 id = rs.getString("id");
+	    		 pw = rs.getString("pw");
+	    		 name = rs.getString("name");
+	    		 phone = rs.getString("phone");
+	    		 member = new Member(id,pw,name,phone);
+	    	 }
+	    }catch(Exception e){
+	    	e.printStackTrace();
+	    }finally{
+	    	try{
+	    		if(rs!=null) rs.close();
+	    		if(pstmt!=null) pstmt.close();
+	    		if(conn!=null) conn.close();
+	    	}catch(Exception e2){e2.printStackTrace();}
+	    }
+		return member;
+	}//selectMemberOne
+	
 	
 	//회원정보리스트 메소드
 	public ArrayList<Member> selectMember() {
 		ArrayList<Member> list = new ArrayList<>();
 		try{
-	    	 context = new InitialContext();
-	    	 dataSource = (DataSource) context.lookup("java:comp/env/jdbc/Oracle11g");
-	    	 conn = dataSource.getConnection();
+	    	 conn = getConnection();
 	    	 query = "select * from member";
 	    	 pstmt = conn.prepareStatement(query);
 	    	 rs = pstmt.executeQuery();
@@ -52,12 +122,35 @@ public class MemberDao {
 	}//selectMember
 	
 	
+	//회원정보수정 메소드
+	public int updateMember(Member member) {
+		try{
+	    	 conn = getConnection();
+	    	 query = "update member set pw=?,name=?,phone=? where id=?";
+	    	 pstmt = conn.prepareStatement(query);
+	    	 pstmt.setString(1, member.getPw());
+	    	 pstmt.setString(2, member.getName());
+	    	 pstmt.setString(3, member.getPhone());
+	    	 pstmt.setString(4, member.getId());
+	    	 
+	    	 //성공-1, 실패-0
+	    	 result = pstmt.executeUpdate(); //executeUpdate()-insert,update,delete   executeQuery()-select
+	    	 
+	    }catch(Exception e){
+	    	e.printStackTrace();
+	    }finally{
+	    	try{
+	    		if(pstmt!=null) pstmt.close();
+	    		if(conn!=null) conn.close();
+	    	}catch(Exception e2){e2.printStackTrace();}
+	    }
+		return result;
+	}//updateMember
+	
 	//회원정보입력 메소드
 	public int insertMember(Member member) {
 		try{
-	    	 context = new InitialContext();
-	    	 dataSource = (DataSource) context.lookup("java:comp/env/jdbc/Oracle11g");
-	    	 conn = dataSource.getConnection();
+	    	 conn = getConnection();
 	    	 query = "insert into member values(?,?,?,?)";
 	    	 pstmt = conn.prepareStatement(query);
 	    	 pstmt.setString(1, member.getId());
--- jsp0619/src/main/webapp/j0619_main.jsp
+++ jsp0619/src/main/webapp/j0619_main.jsp
@@ -9,12 +9,17 @@
 	<body>
 	  <h2>메인페이지</h2>
 	  <ul>
+	  <% if(session.getAttribute("sessionId")==null){ %>
+	     <h3>로그인을 해주세요.</h3>
 	    <a href="j0619_join.jsp"><li>회원가입</li></a> 
-	    <li>로그인</li>
+	    <a href="j0619_login.jsp"><li>로그인</li></a>
+	  <%}else{%>
+	     <h3><%=(String) session.getAttribute("sessionName") %>님 환영합니다.</h3>
+	    <a href="j0619_memberList.jsp"><li>회원정보리스트</li></a>
+	    <a href="j0619_logout.jsp"><li>로그아웃</li></a>
+	    <a href="j0619_modify.jsp"><li>회원정보수정</li></a>
 	    <li>게시판</li>
-	    <li>회원정보리스트</li>
-	    <li>회원정보수정</li>
-	    <li>로그아웃</li>
+	  <%}%>
 	  </ul>
 	
 	</body>
